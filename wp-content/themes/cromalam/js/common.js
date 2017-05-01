41bb426aadd6a771231b3f94329de581b989a639
Rocklime <sumitgoyal.rocklime@gmail.com>
2017-05-01 15:11:28 +0530

    dfdf

--- wp-content/themes/cromalam/js/common.js
+++ wp-content/themes/cromalam/js/common.js
@@ -3,3 +3,7 @@ var $ = jQuery;
 jQuery('.site-projects__mouse-area').on('click', function(){
     jQuery('#site-projects').removeClass('site-projects--visible');
 });
+
+ jQuery(".form-slider__input-wrap #contact-city").on("focus blur keypress keydown keyup change", function(){
+ 	"" !== jQuery(this).val() && jQuery(this).val().length > 0 ? jQuery(this).parents(".form-slider__input-wrap").addClass("text-entered") : jQuery(this).parents(".form-slider__input-wrap").removeClass("text-entered")
+ });
